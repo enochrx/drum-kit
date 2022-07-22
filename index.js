4e66e46575136f5d8efb5626d458e9f0fedd3bdb
enochrx <drreayy@gmail.com>
2022-07-22 13:00:48 +0100

    keypress

--- index.js
+++ index.js
@@ -4,48 +4,56 @@ const btnClick = () => {
 };
 const numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
+document.addEventListener("keydown", function (event) {
+  action(event.key);
+});
+
 for (let i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     const buttonHTML = this.innerHTML;
 
-    switch (buttonHTML) {
-      case "w":
-        const crash = new Audio("sounds/crash.mp3");
-        crash.play();
-        break;
-      case "a":
-        const kickBass = new Audio("sounds/kick-bass2.mp3");
-        kickBass.play();
-        break;
-      case "s":
-        const snare = new Audio("sounds/snare.mp3");
-        snare.play();
-        break;
-      case "d":
-        const tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
-      case "j":
-        const tom2 = new Audio("sounds/tom-2.mp3");
-        tom2.play();
-        break;
-      case "k":
-        const tom3 = new Audio("sounds/tom-3.mp3");
-        tom3.play();
-        break;
-      case "l":
-        const tom4 = new Audio("sounds/tom-4.mp3");
-        tom4.play();
-        break;
-
-      default:
-        break;
-    }
-    //console.log((this.style.color = "white"));
-    //   const audio = new Audio("sounds/crash.mp3");
-    //   audio.play();
+    action(buttonHTML);
   });
 }
+
+const action = function (key) {
+  switch (key) {
+    case "w":
+      const crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "a":
+      const kickBass = new Audio("sounds/kick-bass2.mp3");
+      kickBass.play();
+      break;
+    case "s":
+      const snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "d":
+      const tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "j":
+      const tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "k":
+      const tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "l":
+      const tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    default:
+      "Not a valid input";
+      break;
+  }
+};
+
+//console.log(action);
 
 //////////////////////////////////////////////////////////////////////////////
 //Side Knowledge
